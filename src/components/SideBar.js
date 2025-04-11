bf9ef0d7bff34b52e8f6d481ab469ac0f8bc4824
Taylor Albright <jtayloralbright@gmail.com>
2025-04-11 18:40:29 -0500

    committing changes before refactoring navbar links

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -5,21 +5,36 @@ import { Link, NavLink as SideBarLink, useLocation } from 'react-router-dom'
 import { FaCog, FaBookmark, FaSignOutAlt } from 'react-icons/fa'
 
 export default function SideBar() {
+    const resetSideBar = 768
     const [isCollapsed, setIsCollapsed] = useState(false)
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= resetSideBar);
 
     const toggleSideBar = () => {
         setIsCollapsed(!isCollapsed)
     }
 
     const location = useLocation()
+    console.log(location.pathname)
 
     function activeLink(path) {
         return location.pathname === path
     }
 
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth <= resetSideBar) {
+                setIsMobile(true)
+            } else {
+                setIsMobile(false)
+            }
+        };
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+      }, []);
+
     return (
         <div className={`sideBar ${isCollapsed ? "collapsed" : ""}`}>
-            <button className="sideBarToggle" onClick={toggleSideBar}>☰</button>
+            <button className={`sideBarToggle ${isMobile ? "isMobile" : ""}`} onClick={toggleSideBar}>☰</button>
             <ul style={{marginLeft: "2.0rem", marginRight: "0.4rem", padding: "0", listStyleType: "none"}} className="d-flex flex-column">
                 <li>
                     {/* <SideBarLink to="savedPosts" className={({ isActive }) => isActive ? "sideBar-link active" : "sideBar-link"}><FaBookmark /> Saved Posts</SideBarLink> */}
