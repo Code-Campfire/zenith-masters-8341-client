4fdd4852be9dafbcc9b89a17051d4326f9ed6640
Taylor Albright <jtayloralbright@gmail.com>
2025-04-11 18:00:03 -0500

    Improved responsiveness cohesion between side and navbar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,37 +1,29 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { useEffect, useState } from 'react'
 import '../styles/SideBar.css'
-import { NavLink as SideBarLink } from 'react-router-dom'
+import { Link, NavLink as SideBarLink, useLocation } from 'react-router-dom'
 import { FaCog, FaBookmark, FaSignOutAlt } from 'react-icons/fa'
 
 export default function SideBar() {
-    const resetSideBar = 894
     const [isCollapsed, setIsCollapsed] = useState(false)
-    const [isMobile, setIsMobile] = useState(window.innerWidth <= resetSideBar);
 
     const toggleSideBar = () => {
         setIsCollapsed(!isCollapsed)
     }
 
-    useEffect(() => {
-        const handleResize = () => {
-            const isNowMobile = window.innerWidth <= resetSideBar
-            setIsMobile(isNowMobile)
-            if (!isNowMobile) {
-                setIsCollapsed(false)
-            }
-        };
-    
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-      }, []);
+    const location = useLocation()
+
+    function activeLink(path) {
+        return location.pathname === path
+    }
 
     return (
         <div className={`sideBar ${isCollapsed ? "collapsed" : ""}`}>
             <button className="sideBarToggle" onClick={toggleSideBar}>☰</button>
             <ul style={{marginLeft: "2.0rem", marginRight: "0.4rem", padding: "0", listStyleType: "none"}} className="d-flex flex-column">
                 <li>
-                    <SideBarLink to="savedPosts" className={({ isActive }) => isActive ? "sideBar-link active" : "sideBar-link"}><FaBookmark /> Saved Posts</SideBarLink>
+                    {/* <SideBarLink to="savedPosts" className={({ isActive }) => isActive ? "sideBar-link active" : "sideBar-link"}><FaBookmark /> Saved Posts</SideBarLink> */}
+                    <Link to="savedPosts" className={ activeLink("/savedPosts") ? "sideBar-link active" : "sideBar-link"}><FaBookmark /> Saved Posts</Link>
                 </li>
                 <li>
                     <SideBarLink to="settings" className={({ isActive }) => isActive ? "sideBar-link active" : "sideBar-link"}><FaCog /> Settings</SideBarLink>
