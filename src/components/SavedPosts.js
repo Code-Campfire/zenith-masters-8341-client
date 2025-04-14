f8520687277d312c340ab3ac22de8b16dbf2ef1a
Taylor Albright <jtayloralbright@gmail.com>
2025-04-14 15:11:41 -0500

    Setup login and sign up views and page styling

---
+++ src/components/SavedPosts.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function SavedPosts() {
+  
+  return (
+    <div className='grid-container'>
+      <div className="title"> Saved Posts</div>
+    </div>
+  );
+}
