3ecc47250f488e1f47b9b37e8c0247d04447fa13
Taylor Albright <jtayloralbright@gmail.com>
2025-04-16 19:11:48 -0500

    Implement cleaned up SignUp and Login components

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,50 +1,58 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { useEffect, useState } from 'react'
 import '../styles/SideBar.css'
-import { Link, NavLink as SideBarLink, useLocation } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { FaCog, FaBookmark, FaSignOutAlt } from 'react-icons/fa'
 
 export default function SideBar() {
-    const resetSideBar = 768
-    const [isCollapsed, setIsCollapsed] = useState(false)
-    const [isMobile, setIsMobile] = useState(window.innerWidth <= resetSideBar);
+	const resetSideBar = 768
+	const [isCollapsed, setIsCollapsed] = useState(true)
+	const [isMobile, setIsMobile] = useState(window.innerWidth <= resetSideBar)
 
-    const toggleSideBar = () => {
-        setIsCollapsed(!isCollapsed)
-    }
+	const toggleSideBar = () => {
+		setIsCollapsed(!isCollapsed)
+	}
 
-    const { pathname } = useLocation()
+	const { pathname } = useLocation()
 
-    function activeLink(path) {
-        return pathname === path
-    }
+	function activeLink(path) {
+		return pathname === path
+	}
 
-    useEffect(() => {
-        const handleResize = () => {
-            if (window.innerWidth <= resetSideBar) {
-                setIsMobile(true)
-            } else {
-                setIsMobile(false)
-            }
-        };
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-      }, []);
-
-    return (
-        <div className={`sideBar ${isCollapsed ? "collapsed" : ""}`}>
-            <button className={`sideBarToggle ${isMobile ? "isMobile" : ""}`} onClick={toggleSideBar}>☰</button>
-            <ul className="d-flex flex-column">
-                <li className={activeLink("/posts/saved") ? "active" : ""}>
-                    <Link to="posts/saved" className="sideBar-link"><FaBookmark /> Saved Posts</Link>
-                </li>
-                <li className={activeLink("/settings") ? "active" : ""}>
-                    <Link to="settings" className="sideBar-link"><FaCog /> Settings</Link>
-                </li>
-                <li className={activeLink("/logout") ? "active" : ""}>
-                    <Link to="logout"  className="sideBar-link"><FaSignOutAlt /> Logout</Link>
-                </li>
-            </ul>
-        </div>
-    )
+	useEffect(() => {
+		const handleResize = () => {
+			if (window.innerWidth <= resetSideBar) {
+				setIsMobile(true)
+			} else {
+				setIsMobile(false)
+			}
+		}
+		window.addEventListener('resize', handleResize)
+		return () => window.removeEventListener('resize', handleResize)
+	}, [])
+	//
+	return (
+		<div className={`sideBar ${isCollapsed ? 'collapsed' : ''}`}>
+			<button className={`sideBarToggle ${isMobile ? 'isMobile' : ''}`} onClick={toggleSideBar}>
+				☰
+			</button>
+			<ul className="d-flex flex-column">
+				<li className={activeLink('/posts/saved') ? 'active' : ''}>
+					<Link to="posts/saved" className="sideBar-link">
+						<FaBookmark /> Saved Posts
+					</Link>
+				</li>
+				<li className={activeLink('/settings') ? 'active' : ''}>
+					<Link to="settings" className="sideBar-link">
+						<FaCog /> Settings
+					</Link>
+				</li>
+				<li className={activeLink('/login') ? 'active' : ''}>
+					<Link to="login" className="sideBar-link">
+						<FaSignOutAlt /> Logout
+					</Link>
+				</li>
+			</ul>
+		</div>
+	)
 }
