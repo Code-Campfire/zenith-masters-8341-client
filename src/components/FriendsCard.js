4581b7740dbafba3e9c7088f0750a28116d5603c
Taylor Albright <jtayloralbright@gmail.com>
2025-04-24 15:01:32 -0500

    Add basic styling

--- src/components/FriendsCard.js
+++ src/components/FriendsCard.js
@@ -5,7 +5,13 @@ export default function FriendsCard({ friend, index }) {
 		<div key={index} className="friends-card">
 			<img src="https://upload.wikimedia.org/wikipedia/en/archive/b/b1/20210811082420%21Portrait_placeholder.png" alt="" />
 			<div>{friend.name}</div>
-			<button>Button</button>
+			<button>
+				<picture>
+					<source srcset="/add-friend.svg" />
+					<img alt="Friend Icon" src="/add-friend.svg" style={{ width: '30px', height: '25px' }} />
+				</picture>
+				<h6>Add Friend</h6>
+			</button>
 		</div>
 	)
 }
--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -20,9 +20,23 @@ export default function FriendsList() {
 		{ name: 'Clodbo' },
 		{ name: 'Radkipper' },
 		{ name: 'Grandpa' },
+		{ name: 'Grandpa' },
+		{ name: 'Grandpa' },
+		{ name: 'Grandpa' },
+		{ name: 'Grandpa' },
+		{ name: 'Grandpa' },
+		{ name: 'Grandpa' },
+		{ name: 'Grandpa' },
+		{ name: 'Grandpa' },
+		{ name: 'Grandpa' },
+		{ name: 'Grandpa' },
+		{ name: 'Grandpa' },
+		{ name: 'Grandpa' },
+		{ name: 'Grandpa' },
+		{ name: 'Billy Nickles' },
 	])
 	const [currentPage, setCurrentPage] = useState(1)
-	const itemsPerPage = 8
+	const itemsPerPage = 4
 	const numberOfPages = Math.ceil(friends.length / itemsPerPage)
 	const startIndex = (currentPage - 1) * itemsPerPage
 	const currentItems = friends.slice(startIndex, startIndex + itemsPerPage)
@@ -41,15 +55,21 @@ export default function FriendsList() {
 		<div className="friends-list-container">
 			<div className="friends-list-nav">
 				<div className="top-nav">
-					<div>Friends</div>
-					<div>Suggested Friends</div>
+					<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+						<picture>
+							<source srcset="/friend.svg" />
+							<img alt="Friend Icon" src="/friend.svg" style={{ width: '30px' }} />
+						</picture>
+						<h4>Friends</h4>
+					</div>
+					<div>Suggest Friends</div>
 				</div>
 				<div className="bot-nav">
-					<div>All Friends</div>
-					<div>Recently Added</div>
-					<div>People You May Know</div>
-					<div>Followers</div>
-					<div>Search Bar</div>
+					<h6>All Friends</h6>
+					<h6>Recently Added</h6>
+					<h6>People You May Know</h6>
+					<h6>Followers</h6>
+					<h6>Search Bar</h6>
 				</div>
 			</div>
 			<div style={{ display: 'flex', flexDirection: 'column' }}>
@@ -59,21 +79,21 @@ export default function FriendsList() {
 					})}
 				</div>
 				<div className="friends-list-pagination">
-					<div onClick={next} style={{ border: '2px solid black', width: '60px', alignSelf: 'center', cursor: 'pointer' }}>
-						Next
-					</div>
+					<button onClick={previous} style={{ border: '2px solid black', width: '60px', alignSelf: 'center', cursor: 'pointer' }}>
+						{'<'}
+					</button>
 					<div className="pagination-numbers-container">
 						{Array.from({ length: numberOfPages }).map((item, index) => {
 							return (
-								<div onClick={() => goToPage(index + 1)} style={{ border: '2px solid grey', width: '40px', cursor: 'pointer' }}>
+								<button onClick={() => goToPage(index + 1)} style={{ backgroundColor: index + 1 === currentPage ? 'lightgrey' : '' }}>
 									{index + 1}
-								</div>
+								</button>
 							)
 						})}
 					</div>
-					<div onClick={previous} style={{ border: '2px solid black', width: '80px', alignSelf: 'center', cursor: 'pointer' }}>
-						Previous
-					</div>
+					<button onClick={next} style={{ border: '2px solid black', width: '60px', alignSelf: 'center', cursor: 'pointer' }}>
+						{'>'}
+					</button>
 				</div>
 			</div>
 		</div>
