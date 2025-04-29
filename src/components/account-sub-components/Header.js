53a8a59ebb6ef8168e896b66623eadad9e09e85c
Jovanni Feliz <feliz.jovanni@gmail.com>
2025-04-28 21:07:24 -0500

    Added link buttons with effects and a placeholder for cards on DOM

--- src/components/account-sub-components/Header.js
+++ src/components/account-sub-components/Header.js
@@ -1,15 +1,19 @@
 import '../../styles/account-sub-components/header.css';
+import React, { useState } from 'react';
 import AddFriendBtn from './AddFriendBtn';
 import AddToStoryBtn from './AddToStoryBtn';
 import EditProfileBtn from './EditProfileBtn';
 import ProfileImage from './ProfileImage';
 
 export default function Header({ friendListAmount, userObject }) {
+  const [currentPosts, setCurrentPosts] = useState([]);
+  const [currentComponent, setCurrentComponent] = useState('posts');
+
   let userID = true;
 return (
   <>
     <div id="header"> 
-      <div id='coverBackground'>Cover Photo</div>
+      <div id='coverBackground'>Insert Cover Photo</div>
       <div id='main'>
         <div className='profile-pic-area'>
           <ProfileImage />
@@ -25,8 +29,35 @@ return (
             <EditProfileBtn />
           </div>
         </div>
-      <div id='links'>Links</div>
+      <div id='links'>
+        <button id='posts-btn' className='links-btn' onClick={() => {
+          setCurrentComponent('posts');
+          document.getElementById('about-btn').classList.remove('clicked');
+          document.getElementById('friends-btn').classList.remove('clicked');
+          document.getElementById('posts-btn').classList.add('clicked');
+        }}>Posts</button>
+        <button id='about-btn' className='links-btn' onClick={() => {
+          setCurrentComponent('about');
+          document.getElementById('posts-btn').classList.remove('clicked');
+          document.getElementById('friends-btn').classList.remove('clicked');
+          document.getElementById('about-btn').classList.add('clicked');
+        }}>About</button>
+        <button id='friends-btn' className='links-btn' onClick={() => {
+          setCurrentComponent('friends');
+          document.getElementById('posts-btn').classList.remove('clicked');
+          document.getElementById('about-btn').classList.remove('clicked');
+          document.getElementById('friends-btn').classList.add('clicked');
+        }}>Friends</button>
+      </div>
+      <div id='account-root' />
+      <div>
+        {
+          (currentComponent === 'posts' && <p>Posts Component Placeholder</p>) 
+          || (currentComponent === 'about' && <p>About Page Component Placeholder </p>) 
+          || (currentComponent === 'friends' && <p>Friends List Component Placeholder</p>) 
+        }
+      </div>
     </div>
   </>
-  );
+);
 }
